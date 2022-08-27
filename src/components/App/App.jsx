9073562d12c8a003e8c476734f7610f5365aea7f
Chaitanya <chaitu.vsh@gmail.com>
2022-08-27 23:07:28 +0530

    Basic setup

---
+++ src/components/App/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import LandingPage from '../LandingPage/LandingPage';
+
+const App = () => {
+  return (
+    <>
+        <div>App</div>
+    </>
+  )
+}
+
+export default App;
