698cb8439f520447b832e6ab824d67b13f2e22fc
Chaitanya <chaitu.vsh@gmail.com>
2022-08-31 23:49:49 +0530

    [COMPONENTS] [NavBar] Creaing a basic navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import styles from "./NavBar.module.css";
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <div className={styles.navBarWrapper}>
+      <p>Framing Memories</p>
+      <ul>
+        <li>Gallery</li>
+        <li>Services</li>
+        <li>Contact</li>
+      </ul>
+    </div>
   )
 }
 
