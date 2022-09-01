63e7ef22ee26297477de7abd91a733276cee5ac1
Chaitanya <chaitu.vsh@gmail.com>
2022-09-01 23:00:34 +0530

    [COMPONENTS] [NavBar] Adding click functionality for mobile menu hamburger icon

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // Icons
 import { AiOutlineMenu } from "react-icons/ai";
@@ -6,16 +6,23 @@ import { AiOutlineMenu } from "react-icons/ai";
 import styles from "./NavBar.module.css";
 
 const NavBar = () => {
+
+  const [showMenu, setShowMenu] = useState(false);
+
+  const _showMenu = () => {
+    setShowMenu(!showMenu)
+  }
+
   return (
     <div className={styles.navBarWrapper}>
-      <p>Framing Memories</p>
-      <ul>
+      <h2>Framing Memories</h2>
+      <ul className={showMenu ? styles.show : ""}>
         <li>Gallery</li>
         <li>Services</li>
         <li>Contact</li>
       </ul>
       {/* Show the icon only on screens that go below 500px */}
-      <AiOutlineMenu className={styles.menuIcon}/>
+      <AiOutlineMenu className={styles.menuIcon} onClick={_showMenu}/>
     </div>
   )
 }
